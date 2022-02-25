7a96148bb7e618dda628a508276cd61c12de32d9
randy430e <randy430e@gmail.com>
2022-02-25 15:32:42 -0500

    login form with button that hides it

---
+++ app.js
@@ -0,0 +1,11 @@
+/*-----Welcome-----*/
+var login = document.getElementById("login_container");
+
+function showLogin(){
+    if(login.style.display === "none"){
+        login.style.display = "block";
+    }
+    else{
+        login.style.display = "none";
+    }
+}
