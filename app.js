1f7bd0cb4f7f55d2f588b1db5c45225b83619e04
randy430e <randy430e@gmail.com>
2022-03-04 16:01:53 -0500

    further validation for sign-up form

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ var signupForm = document.getElementById("signup_container");
 var username = document.forms['loginform']['username'];
 var password = document.forms['loginform']['password'];
 
-var userCreate = document.forms['signupform']['userCreate'];
-var passCreate = document.forms['signupform']['passCreate'];
-var passConfirm = document.forms['signupform']['passConfirm'];
+var userCreateIn = document.forms['signupform']['userCreate'];
+var passCreateIn = document.forms['signupform']['passCreate'];
+var passConfirmIn = document.forms['signupform']['passConfirm'];
+var userCreate = document.getElementById("userCheck");
+var passCreate = document.getElementById("passCheck");
+var passConfirm = document.getElementById("passCheck2");
 
 var userError = document.getElementById("user_error");
 var passError = document.getElementById("pass_error");
 
 username.addEventListener('input', userVerify);
 password.addEventListener('input', passVerify);
+
+userCreate.addEventListener('input', userChecker);
+passCreate.addEventListener('input', passChecker);
+passConfirm.addEventListener('keyup', passChecker2);
 
 // Hides login and sign-up forms 
 function showLogin(){
@@ -37,7 +44,7 @@ function showSignup(){
 }
 
 // Validates inputs for the forms 
-function validated(){
+function validateLogin(){
     if (username.value.length < 5) {
         username.style.border = "1px solid red";
         userError.style.display = "block";
@@ -65,5 +72,48 @@ function passVerify(){
         password.style.border = "1px solid silver";
         passError.style.display = "none";
         return true;
+    }
+}
+
+function validateSignup(){
+    if (userCreateIn.value.length < 5) {
+        userCreateIn.style.border = "1px solid red";
+        userCreateIn.focus();
+        return false;
+    }
+    if (passCreateIn.value.length < 6) {
+        passCreateIn.style.border = "1px solid red";
+        passCreateIn.focus();
+        return false;
+    }
+    if (passConfirmIn.value != passCreateIn.value) {
+        passConfirmIn.style.border = "1px solid red";
+        passConfirmIn.focus();
+        return false;
+    }
+}
+
+function userChecker(){
+    if (userCreateIn.value.length >= 5) {
+        userCreate.style.color = "green";
+        return true;
+    }
+}
+
+function passChecker(){
+    if (passCreateIn.value.length >= 6) {
+        passCreate.style.color = "green";
+        return true;
+    }
+}
+
+function passChecker2(){
+    if (passConfirmIn.value == passCreateIn.value) {
+        passConfirm.style.display = "none";
+        return true;
+    }
+    else{
+        passConfirm.style.display = "block";
+        return false;
     }
 }
