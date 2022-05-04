1d8eed82dce0146b21a19837e4de0c5fe7ed3619
randy430e <randy430e@gmail.com>
2022-05-04 14:55:27 -0400

    js for counter visibility

---
+++ post.js
@@ -0,0 +1,38 @@
+const placeholder = document.querySelector('.placeholder');
+const input = document.querySelector('.edit-input');
+const counterLimit = document.querySelector('.counter-limit');
+const postBtn = document.querySelector('.post-btn');
+
+let limit = 300;
+
+input.addEventListener('keyup',function(){
+    const inputLength = input.textContent.length;
+    if(inputLength > 0){
+        placeholder.style.display = 'none';
+        const limitNum = counterLimit.textContent = limit - inputLength;
+        counterLimit.style.visibility = 'visible';
+        postBtn.classList.add('active');
+
+        if(limitNum < 0){
+            counterLimit.style.color = 'red';
+            postBtn.classList.remove('active');
+        }else{
+            counterLimit.style.color = '#000'
+        }
+    }else{
+        placeholder.style.display = 'block';
+        counterLimit.textContent = limit;
+        counterLimit.style.visibility = 'hidden';
+        postBtn.classList.remove('active');
+    }
+})
+
+postBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    let counterVal = counterLimit.innerHTML;
+    if(counterVal > 0){
+        return true;
+    }else{
+        return false;
+    }
+})
